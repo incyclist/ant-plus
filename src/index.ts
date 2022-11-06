cee6a64e275b5dc3c329fb4164a33d5504838296
Florian PASTEUR <florian.c.p@pm.me>
2022-11-06 10:58:26 +0100

    [Bugfix] Missing export for AntDevice on index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import Channel from './ant-channel'
 export {Channel}
 export {Constants} from './consts'
 export {Messages} from './messages'
+export {AntDevice} from './ant-device';
 export * from './sensors'
 
 export {ChannelProps, IAntDevice, IChannel, ISensor} from './types'
